a8971423c82cdbbe91938cafc108dff32d6ecb9a
middletaq@gmail.com <middletaq@gmail.com>
2023-04-30 22:30:59 -0400

    anime

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://taquan:apple123@sneakers.p0szlqh.mongodb.net/HotTakes?retryWrites=true&w=majority', 
+    'dbName': 'Anime'
+};
